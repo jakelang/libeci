17632c3611dbd61925c4f4f5e4408167e89c9410
Jake Lang <phazon@protonmail.com>
2018-07-10 19:18:52 -0400

    implement better eci checklist struct

--- src/context.rs
+++ src/context.rs
@@ -1,10 +1,5 @@
-use eci::CheckStatus;
-
 pub struct EcicContext {
     code: Vec<u8>,
-    check_export_main: CheckStatus,
-    check_export_mem: CheckStatus,
-    check_imports: CheckStatus 
 }
 
 impl EcicContext {
@@ -18,18 +13,12 @@ impl EcicContext {
     pub fn empty() -> Self {
         EcicContext {
             code: Vec::new(),
-            check_export_main: CheckStatus::Unknown,
-            check_export_mem: CheckStatus::Unknown,
-            check_imports: CheckStatus::Unknown
         }
     }
 
     pub fn from_vec(input: &Vec<u8>) -> Self {
         EcicContext {
             code: input.clone(),
-            check_export_main: CheckStatus::Unknown,
-            check_export_mem: CheckStatus::Unknown,
-            check_imports: CheckStatus::Unknown
         }
     }
 
@@ -41,12 +30,10 @@ impl EcicContext {
 #[cfg(test)]
 mod tests {
     use context::EcicContext;
-    use eci::CheckStatus;
 
     #[test]
     fn empty_code() {
         let ctx = EcicContext::new();
-        assert_eci_checks_initialized(&ctx);
         assert!(ctx.code.is_empty());
     }
     
@@ -54,25 +41,7 @@ mod tests {
     fn some_code() {
         let wasm = vec!(0x00, 0x77, 0x61, 0x73, 0x6d);
         let ctx = EcicContext::from_vec(&wasm);
-        assert_eci_checks_initialized(&ctx);
         assert_eq!(5, wasm.len());
         assert_eq!(5, ctx.code_len());
-    }
-
-    fn assert_eci_checks_initialized(ctx: &EcicContext) {
-        match ctx.check_export_main {
-            CheckStatus::Unknown => (),
-            _ => assert!(false)
-        }
-
-        match ctx.check_export_mem {
-            CheckStatus::Unknown => (),
-            _ => assert!(false)
-        }
-
-        match ctx.check_imports {
-            CheckStatus::Unknown => (),
-            _ => assert!(false)
-        }
     }
 }
--- src/eci.rs
+++ src/eci.rs
@@ -1,11 +1,57 @@
+use std::collections::HashMap;
+
 /*
  * Enum describing the state of a check on an ECI property.
  * This is always initialized as "Unknown", and changed during
  * the check phase to reflect its degree of ECI conformance.
  */
+#[derive(Copy, Clone)]
 pub enum CheckStatus {
     Unknown,
     Nonexistent,
     Malformed,
     Good,
+}
+
+pub struct EciChecklist {
+    checklist: HashMap<String, CheckStatus>
+}
+
+impl EciChecklist {
+    pub fn new() -> Self {
+        EciChecklist {
+            checklist: HashMap::new()
+        }
+    }
+
+    pub fn eci_default() -> Self {
+        let checks: HashMap<String, CheckStatus> = 
+            [("export-main".to_string(), CheckStatus::Unknown),
+             ("export-memory".to_string(), CheckStatus::Unknown),
+             ("eei-imports".to_string(), CheckStatus::Unknown)]
+            .iter().cloned().collect();
+
+        EciChecklist {
+            checklist: checks
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use eci::*;
+
+    #[test]
+    fn empty_checklist() {
+        let checks = EciChecklist::new();
+        assert!(checks.checklist.is_empty());
+    }
+
+    #[test]
+    fn default_checks() {
+        let checks = EciChecklist::eci_default();
+        assert!(checks.checklist.contains_key(&"export-main".to_string()));
+        assert!(checks.checklist.contains_key(&"export-memory".to_string()));
+        assert!(checks.checklist.contains_key(&"eei-imports".to_string()));
+    }
 }
