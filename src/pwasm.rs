5f03d56daec9c2dbcd9025153ca9ab0902206808
Jake Lang <phazon@protonmail.com>
2018-07-18 19:01:21 -0400

    modify export_index_by_name to properly return nothing if an export is not found

--- src/pwasm.rs
+++ src/pwasm.rs
@@ -1,7 +1,7 @@
 use parity_wasm::elements::{Module, Type, FunctionType, Internal, External};
 
 /* Borrowed from pwasm examples */
-fn type_by_index(module: &Module, index: usize) -> FunctionType {
+fn func_type_by_index(module: &Module, index: usize) -> FunctionType {
 
 	let function_section = module.function_section().expect("No function section found");
 	let type_section = module.type_section().expect("No type section found");
@@ -24,20 +24,22 @@ fn type_by_index(module: &Module, index: usize) -> FunctionType {
 	}
 }
 
-fn export_index_by_name(module: &Module, name: &str) -> usize {
-    assert!(has_export_section(module));
-    let idx: usize = match *module.export_section().unwrap().entries().iter()
-        .find(|export|
-                if export.field() == name { true } else { false })
-        .unwrap()
-        .internal() { //Is there any way to do this more simply?
-            Internal::Function(index) => index as usize,
-            Internal::Memory(index) => index as usize,
-            Internal::Global(index) => index as usize,
-            Internal::Table(index) => index as usize,
-        };
-
-    idx
+fn export_index_by_name(module: &Module, name: &str) -> Option<usize> {
+    if !has_export_section(module) { 
+        return None; 
+    } else {
+        let idx: Option<usize> = match module.export_section().unwrap().entries().iter()
+            .find(|export| if export.field() == name { true } else { false }) {
+                Some(export) => match *export.internal() { //Is there any way to do this more simply?
+                        Internal::Function(index) => Some(index as usize),
+                        Internal::Memory(index) => Some(index as usize),
+                        Internal::Global(index) => Some(index as usize),
+                        Internal::Table(index) => Some(index as usize),
+                }, 
+                None => return None, 
+            };
+        idx
+    }
 }
 
 pub fn has_export_section(module: &Module) -> bool {
